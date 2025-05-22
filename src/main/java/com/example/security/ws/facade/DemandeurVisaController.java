e88046281559c831a226e2213132ad344ede0a1c
dihifatima <dihifatima08@gmail.com>
2025-05-22 17:11:42 +0000

    Mon message de commit

--- src/main/java/com/example/security/ws/facade/DemandeurVisaController.java
+++ src/main/java/com/example/security/ws/facade/DemandeurVisaController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/porteVisa")
+@RequestMapping("/demandeurVisa")
 public class DemandeurVisaController {
     @Autowired
     private DemandeurVisaService service;
